74ea2ed3bc516a763bf544c3a66524953ee39c3d
Vineeth.TR <vineethtrv@gmail.com>
2022-03-13 16:05:40 +0530

    order change

--- assets/data/web.js
+++ assets/data/web.js
@@ -2,18 +2,18 @@ export const web = {
     name: 'Application',
     id: 'app',
     data: [
-        {
-            name: 'CSS Loaders & Spinners',
-            image: './assets/images/Loader-screen.png',
-            url: 'https://cssloaders.github.io/',
-            details: "A usual situation that we all might face while going through websites is to wait for some progress or something to get loaded. A most common solution in such scenarios is to use a loader,spinner to indicate users  This is a library having a collection of such loaders,spinners . There is no image dependencies in this. It's is done using pure CSS. Hence it is easily customization too"
-        },
-        
+                
          {
             name: 'Wordle Helper',
             image: './assets/images/wordle-helper.png',
             url: 'https://vineethtrv.github.io/wordle-helper/',
             details: "Wordle Helper will help players gain an advantage to find the wordle puzzle answer with word suggestions. The word list contains all the possible words that can be answers to the Wordle puzzles. The word list can filter by entering information from Wordle about which letters are correct and incorrect."
+        },
+        {
+            name: 'CSS Loaders & Spinners',
+            image: './assets/images/Loader-screen.png',
+            url: 'https://cssloaders.github.io/',
+            details: "A usual situation that we all might face while going through websites is to wait for some progress or something to get loaded. A most common solution in such scenarios is to use a loader,spinner to indicate users  This is a library having a collection of such loaders,spinners . There is no image dependencies in this. It's is done using pure CSS. Hence it is easily customization too"
         },
         
         {
