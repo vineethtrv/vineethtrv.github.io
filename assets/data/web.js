cf08e36581f5524f1487926ffedf432bee1ab834
Vineeth.TR <vineethtrv@gmail.com>
2022-03-13 15:10:04 +0530

    Wordle Helper added

--- assets/data/web.js
+++ assets/data/web.js
@@ -3,22 +3,31 @@ export const web = {
     id: 'app',
     data: [
         {
-            name: 'Vaccine Bell',
-            image: './assets/images/vaccinebell-seo.png',
-            url: 'https://vaccinebell.in/',
-            details: "Get yourself 🔔 notified about the vaccine availability near you. Using Vaccine Bell, Select your nearest hospital or center and get the latest updates on vaccine availability through Browser Alert, email or WhatsApp"
+            name: 'CSS Loaders & Spinners',
+            image: './assets/images/Loader-screen.png',
+            url: 'https://cssloaders.github.io/',
+            details: "A usual situation that we all might face while going through websites is to wait for some progress or something to get loaded. A most common solution in such scenarios is to use a loader,spinner to indicate users  This is a library having a collection of such loaders,spinners . There is no image dependencies in this. It's is done using pure CSS. Hence it is easily customization too"
         },
+        
+         {
+            name: 'Wordle Helper',
+            image: 'https://vineethtrv.github.io/wordle-helper/images/wordle-helper.jpg',
+            url: 'https://vineethtrv.github.io/wordle-helper/',
+            details: "Wordle Helper will help you to find the wordle puzzle answer with word suggestions"
+        },
+        
         {
             name: 'Add-to-WhatsApp',
             image: 'https://vineethtrv.github.io/Add-to-WhatsApp/images/seo.png',
             url: 'https://vineethtrv.github.io/Add-to-WhatsApp/',
             details: "WhatsApp is the most popular messaging application for everyone. However, the users do face a huge problem when they are trying to text an unknown person without necessarily having to save the number. Add-to-WhatsApp is small PWA app will help user to send whatsApp message without saving unknown number"
         },
+
         {
-            name: 'CSS Loaders & Spinners',
-            image: './assets/images/Loader-screen.png',
-            url: 'https://cssloaders.github.io/',
-            details: "A usual situation that we all might face while going through websites is to wait for some progress or something to get loaded. A most common solution in such scenarios is to use a loader,spinner to indicate users  This is a library having a collection of such loaders,spinners . There is no image dependencies in this. It's is done using pure CSS. Hence it is easily customization too"
+            name: 'Vaccine Bell',
+            image: './assets/images/vaccinebell-seo.png',
+            url: 'https://vaccinebell.in/',
+            details: "Get yourself 🔔 notified about the vaccine availability near you. Using Vaccine Bell, Select your nearest hospital or center and get the latest updates on vaccine availability through Browser Alert, email or WhatsApp"
         },
         {
             name: 'HTML Icons',
