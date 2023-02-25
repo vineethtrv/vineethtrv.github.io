529e91df9d834ef80279676ef2989a7800bf455e
Vineeth T R <vineeth.tr@gds.ey.com>
2023-02-25 22:47:53 +0530

    Console log added

---
+++ assets/js/log.js
@@ -0,0 +1,131 @@
+const see = {
+    get Contact() {
+        contact()
+    },
+    get Follow() {
+        Follow()
+    },
+    get No() {
+        No()
+    },
+};
+
+
+
+setTimeout(() => {
+    console.clear();
+    console.log('%c 👋 Hey are you looking for something 🧐..?  👉', "color:#03a9f4; font-size: 22px", see);
+    console.log('%c > Contact()  \n > Follow() \n > No()', "color:#1aaa4e; font-size: 22px");
+}, 300);
+
+
+
+const contact = CONTACT = Contact = () => {
+    /**
+     
+    😕.?..Oh! You missed ()
+    💡 try:  Contact()
+
+
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+    */
+
+    console.table({ 
+            'Email': 'vineethtrv@gmail.com',
+            'LinkedIn': 'https://www.linkedin.com/in/vineethtrv',
+            'Twitter': 'https://mobile.twitter.com/vineethtrv',
+            'Instagram': 'https://www.instagram.com/vineeth.tr'
+        });
+}
+
+const follow = FOLLOW = Follow = () => {
+    /**
+     
+    😕.?..Oh! You missed ()
+    💡 try:  Contact()
+
+
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+    */
+
+    console.table({ 
+            'Twitter': 'https://mobile.twitter.com/vineethtrv',
+            'Facebook': 'https://www.facebook.com/vinithtrv',
+            'Instagram': 'https://www.instagram.com/vineeth.tr',
+            'Github': 'https://github.com/vineethtrv',
+            'CodePen': 'https://codepen.io/vineethtrv'
+        });
+}
+
+
+const no = NO = No = () => {
+    /**
+     
+    😕.?..Oh! You missed ()
+    💡 try:  No()
+
+
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+     * 
+    */
+
+
+
+    console.log("%c Cool, No worries 🤗 ", "color:orange; font-size: 20px");
+}
+
+
+
